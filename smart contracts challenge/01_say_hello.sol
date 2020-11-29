7e88dbb87367cafbdca0b435c32cd234abd097c7
SigelKoo <352326510@qq.com>
2020-11-29 21:39:38 +0800

    add 01.sol

---
+++ smart contracts challenge/01_say_hello.sol
@@ -0,0 +1,24 @@
+pragma solidity >= 0.4.21 <0.6.0;
+
+contract say_hello {
+    string private greeting;
+    address private owner;
+
+    constructor(string _firstGreeting) {
+        greeting = _firstGreeting;
+        owner = msg.sender;
+    }
+
+    function sayHello() public view returns (string) {
+        if (msg.sender == owner) {
+            return "Hello Daddy";
+        } else {
+            return greeting;
+        }
+    }
+
+    function changeGreeting(string _changeGreeting) public {
+        require(msg.sender == owner);
+        greeting = _changeGreeting;
+    }
+}
