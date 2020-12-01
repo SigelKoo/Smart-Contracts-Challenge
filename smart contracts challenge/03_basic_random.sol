df3a1d49cca0c0bd5628aef6e43b05bf8381b9dd
SigelKoo <352326510@qq.com>
2020-12-01 11:12:23 +0800

    update 03.sol

--- smart contracts challenge/03_basic_random.sol
+++ smart contracts challenge/03_basic_random.sol
@@ -3,7 +3,7 @@ pragma solidity >= 0.4.21 < 0.6.0;
 contract basic_random {
     uint private randNonce = 0;
     function getRandomNumber() public view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce)));
+        uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 99;
         randNonce ++;
         return rand;
     }
