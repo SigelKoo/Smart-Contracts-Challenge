cd90bfc368450e6e2c66c493e0a12687a25b9753
SigelKoo <352326510@qq.com>
2020-12-01 11:00:36 +0800

    add 05.sol

---
+++ smart contracts challenge/05_lottery_no_limit.sol
@@ -0,0 +1,30 @@
+pragma solidity >= 0.4.21 < 0.6.0;
+
+contract lottery_no_limit {
+    address private creater;
+    address[] private participants;
+
+    constructor() {
+        creater = msg.sender;
+    }
+
+    function join() public payable {
+        require(msg.value == 0.1 ether);
+        participants.push(msg.sender);
+    }
+
+    function selectWinner() private returns (address) {
+        require(msg.sender == creater);
+        require(participants.length > 0);
+        address winner = participants[randomNumber()];
+        address thisAddr = address(this);
+        address(uint160(winner)).transfer(address(uint160(thisAddr)).balance);
+        delete participants;
+    }
+
+    function randomNumber() private returns(uint) {
+        uint rand = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % (participants.length);
+        randNonce++;
+        return rand;
+    }
+}
